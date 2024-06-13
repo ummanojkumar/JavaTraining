d2462eeb53a108aedeb650393de0a1d6a8b71352
offline computer <ummanojkumar925@gmail.com>
2024-06-13 11:19:44 +0530

    linear searcha and binary search

--- src/main/java/leetCode/Searching.java
+++ src/main/java/leetCode/Searching.java
@@ -1,4 +1,36 @@
 package leetCode;
 
 public class Searching {
+
+    public static void main(String[] args) {
+//    Searching.linearSearch(new int[]{1,3,5,8,9}, 8);
+
+        System.out.println(Searching.binarySearch(new int[]{1,2,5,6,7,8,9}, 7));
+    }
+
+    static void linearSearch(int[] array, int num) {
+        for(int x=0; x<array.length; x++) {
+            if(num==array[x]) {
+                System.out.println("Index fount at "+ x);
+            }
+        }
+       System.out.println("Index not found");
+    }
+
+   static int binarySearch(int array[], int num) {
+        //arrays should be in ascending order
+        int low = 0;
+        int high = array.length-1;
+        while (low<=high) {
+            int mid = (low+high)/2;
+            if (num==array[mid]) {
+                return mid;
+            } else if (num<array[mid]){
+                high = mid -1;
+            } else if (num>array[mid]) {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
 }
